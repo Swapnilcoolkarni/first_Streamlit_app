9f614b4309fb441cceb8d0930cb24a13bc954307
Swapnilcoolkarni <142492387+Swapnilcoolkarni@users.noreply.github.com>
2023-08-17 16:08:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents New Helathy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach and Rocket smoothy')
+streamlit.text('Hardboiled Fre Range egg')
