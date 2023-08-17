244d3f628279afc7476a08cb663aed5a4f8b7f5d
Swapnilcoolkarni <142492387+Swapnilcoolkarni@users.noreply.github.com>
2023-08-17 16:11:44 +0530

    Update streamlit_app.py
    added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents New Helathy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket smoothy')
-streamlit.text('Hardboiled Fre Range egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale,Spinach and Rocket smoothy')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
